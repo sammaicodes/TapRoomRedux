fb601b19afe7ea76e0efef1c6eec4fecd107ff3d
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-07-02 09:54:39 -0700

    Add content to Keg.js

---
+++ src/components/Keg.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Keg(props){
+  return (
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <h3>{props.brand}</h3>
+      <h3>{props.price}</h3>
+      <p><em>{props.alcoholContent}</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  alcoholContent: PropTypes.string.isRequired
+};
+
+export default Keg;
