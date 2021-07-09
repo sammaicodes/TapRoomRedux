8658a67725d4bade73acb24184352029eb8d93d8
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-07-09 10:53:36 -0700

    Pass 5th test-return default state for formvisible.

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -2,9 +2,9 @@ import * as c from "../actions/ActionTypes"
 
 export default (state = false, action) => {
   switch (action.type) {
-   
+
     default:
-      return "idk"
+      return state
   }
 }
 
