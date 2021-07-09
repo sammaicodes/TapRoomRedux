fe8a832b55b7ee7d386ad3f16fa4534a5d9a16ba
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-07-09 10:58:21 -0700

    Fail 6th test-toggle vis. set to true.

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -3,9 +3,13 @@ import * as c from "../../actions/ActionTypes"
 import formVisibleReducer from '../../reducers/form-visible-reducer'
 
 describe(`formVisibleReducer`, () => {
-  
+
   test(`Should return default state if no action type is recognized`, () => {
     expect(formVisibleReducer(false, { type: null })).toEqual(false)
+  })
+  
+  test(`Should toggle form visibility state to true`, () => {
+    expect(formVisibleReducer(false, { type: c.TOGGLE_FORM })).toEqual(true)
   })
   
 })
