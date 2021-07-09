f5733814cdb55ac909719e866ad258a488a91460
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-07-09 16:21:01 -0700

    Add more styling

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,22 @@ import KegControl from "./KegControl";
 
 function App(){
 
+  const myStyledComponentStyles = {
+    display: 'center',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px',
+    paddingLeft: '200px',
+    paddingRight: '200px',
+    textAlign: 'center'
+  }
+
   return ( 
+    <div style={myStyledComponentStyles}>
     <React.Fragment>
       <Header />
       <KegControl />
     </React.Fragment>
+    </div>
   );
 }
 
