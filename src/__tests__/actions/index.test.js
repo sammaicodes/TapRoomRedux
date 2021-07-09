fb6cb3496f56747427eddfbd106676e54ad21085
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-07-09 09:29:00 -0700

    1st test- addKeg action.

---
+++ src/__tests__/actions/index.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable max-len */
+import * as actions from "../../actions"
+import * as c from "../../actions/ActionTypes"
+
+describe(`Tap Room actions`, () => {
+
+  it(`addKeg should create ADD_KEG action`, () => {
+    expect(actions.addKeg({ name: `Belgian White`, brand: `Blue Moon`, price: `8.00`, alcoholContent: `5%`, pintStock:`5`, id: 1 })).toEqual({
+      type: c.ADD_KEG,
+      name: `Belgian White`,
+      brand: `Blue Moon`,
+      price: `8.00`,
+      alcoholContent: `5%`, 
+      pintStock:`5`,
+      id: 1,
+    })
+  })
+})
---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import * as c from "./ActionTypes"
+
+export const addKeg = keg => {
+  const { name, brand, price, alcoholContent, pintStock, id } = keg
+  return {
+    type: c.ADD_KEG,
+    name: `Belgian White`,
+    brand: `Blue Moon`,
+    price: `8.00`,
+    // alcoholContent: `5%`, 
+    pintStock:`5`,
+    id: 1,
+  }
+}
